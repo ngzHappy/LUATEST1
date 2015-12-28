44a814504159279dea38561991f34e7bd0d5cc28
ngzHappy <819869472@qq.com>
2015-12-28 09:29:28 +0800

    +test

--- TestQVariant/main.cpp
+++ TestQVariant/main.cpp
@@ -189,10 +189,214 @@ void foo( int a,int b ) {
     std::cout<<b<<std::endl;
 }
 
+#include <initializer_list>
+
+void fxx111( const std::initializer_list<QVariant> & v ) {
+    for ( const auto & i:v ) {
+        qDebug()<<i;
+    }
+}
+
+void fxx222() {
+
+    QList<int> t{1,2,3};
+    
+    QVariant v=QVariant::fromValue( t );
+
+    auto it = v.value< QSequentialIterable >();
+
+    for ( const auto & i:it ) {
+        qDebug()<<i;
+    }
+
+}
+
+void fxx333() {
+
+    std::list<int> t{1,2,3};
+
+    QVariant v=QVariant::fromValue( t );
+
+    auto it = v.value< QSequentialIterable >();
+
+    for ( const auto & i:it ) {
+        qDebug()<<i;
+    }
+
+}
+
+class AAA {
+public:
+    AAA() {}
+    AAA(int) {}
+    ~AAA() {}
+
+    AAA(const AAA &) { std::cout<<"-1"<<std::endl; }
+    AAA(AAA &&) { std::cout<<"-2"<<std::endl;  }
+
+    AAA&operator=(const AAA &) { std::cout<<"-3"<<std::endl; return *this; }
+    AAA&operator=(AAA &&) { std::cout<<"-4"<<std::endl; return *this; }
+
+};
+
+Q_DECLARE_METATYPE( AAA )
+
+template<typename T>
+class NList : 
+    public std::shared_ptr< std::list<T> >{
+    typedef std::shared_ptr< std::list<T> > Super;
+
+    typedef std::list<T> C_;
+    C_ * data_() { return this->get(); }
+    const C_ * data_()const { return this->get(); }
+    
+public:
+    typedef typename C_::value_type value_type;
+    typedef typename C_::const_iterator const_iterator;
+public:
+    template<typename ... Args>
+    NList( Args && ... args ):Super( std::make_shared<Super>(std::forward<Args>(args)...) ){}
+    NList():Super( new std::list<T> ) {}
+
+    //auto size()const { return data_()->size(); }
+    auto begin()const { return data_()->begin(); }
+    auto end() const { return data_()->end(); }
+};
+
+#include <map>
+Q_DECLARE_SEQUENTIAL_CONTAINER_METATYPE( NList )
+
+namespace cct{
+
+template<typename T,typename U>
+class Map : public std::shared_ptr< std::map<T,U> > {
+private:
+    typedef std::shared_ptr< std::map<T,U> > Super;
+public:
+
+    Map( decltype(nullptr) ) {}
+    Map() :Super(new std::map<T,U> ){}
+    Map(const Map &)=default;
+    Map(Map &&)=default;
+    Map(Super && o):Super( std::move(o) ) {}
+    Map(const Super & o):Super( o ) {}
+    Map(const std::map<T,U> & o):Super( new std::map<T,U>( o ) ) {}
+    Map(std::map<T,U> && o):Super( new std::map<T,U>( std::move(o) ) ) {}
+
+    Map&operator=(const Map&)=default;
+    Map&operator=(Map&&)=default;
+
+    Map copy() const { return Map( *(*this) ); }
+    Map unique_copy() const { if (this->use_count()<2) { return *this; }return copy(); }
+private:
+    typedef std::map<T,U> AssociativeContainer;
+    AssociativeContainer * data_() { return this->get(); }
+    const AssociativeContainer * data_()const { return this->get(); }
+public:
+    typedef typename AssociativeContainer::key_type key_type;
+    typedef typename AssociativeContainer::mapped_type mapped_type;
+    typedef typename AssociativeContainer::const_iterator const_iterator;
+    auto begin()const { return data_()->begin(); }
+    auto end()const { return data_()->end(); }
+    auto begin() { return data_()->begin(); }
+    auto end() { return data_()->end(); }
+    auto find(const key_type & v) const { return data_()->find(v); }
+};
+
+}
+
+Q_DECLARE_ASSOCIATIVE_CONTAINER_METATYPE( cct::Map )
+
+namespace cct{
+
+template<typename T,typename U>
+class Multimap : public std::shared_ptr< std::multimap<T,U> > {
+private:
+    typedef std::shared_ptr< std::multimap<T,U> > Super;
+public:
+
+    Multimap( decltype(nullptr) ) {}
+    Multimap() :Super(new std::multimap<T,U> ){}
+    Multimap(const Multimap &)=default;
+    Multimap(Multimap &&)=default;
+    Multimap(Super && o):Super( std::move(o) ) {}
+    Multimap(const Super & o):Super( o ) {}
+    Multimap(const std::multimap<T,U> & o):Super( new std::multimap<T,U>( o ) ) {}
+    Multimap(std::multimap<T,U> && o):Super( new std::multimap<T,U>( std::move(o) ) ) {}
+
+    Multimap&operator=(const Multimap&)=default;
+    Multimap&operator=(Multimap&&)=default;
+
+    Multimap copy() const { return Multimap( *(*this) ); }
+    Multimap unique_copy() const { if (this->use_count()<2) { return *this; }return copy(); }
+private:
+    typedef std::multimap<T,U> AssociativeContainer;
+    AssociativeContainer * data_() { return this->get(); }
+    const AssociativeContainer * data_()const { return this->get(); }
+public:
+    typedef typename AssociativeContainer::key_type key_type;
+    typedef typename AssociativeContainer::mapped_type mapped_type;
+    typedef typename AssociativeContainer::const_iterator const_iterator;
+    auto begin()const { return data_()->begin(); }
+    auto end()const { return data_()->end(); }
+    auto begin() { return data_()->begin(); }
+    auto end() { return data_()->end(); }
+    auto find(const key_type & v) const { return data_()->find(v); }
+};
+
+}
+
+Q_DECLARE_ASSOCIATIVE_CONTAINER_METATYPE( cct::Multimap )
+
+#include <QSet>
+void fxx777() {
+    QSet<int> fse;
+    fse.insert(12); fse.insert(13);
+    QVariant v=QVariant::fromValue( fse );
+
+    auto it = v.value<QSequentialIterable>();
+    for ( const auto & i:it ) {
+        qDebug()<< i ;
+    }
+}
+
+void fxx444() {
+
+    NList<AAA> t ;
+    t->push_back(1);
+    t->push_back(2);
+    t->push_back(3);
+  
+    QVariant v=QVariant::fromValue( t );
+
+    auto it = v.value< QSequentialIterable >();
+    
+    int xvalue=0;
+    for ( const auto & i:it ) {
+        qDebug()<<xvalue++;
+    }
+
+}
+
+void fxx5555() {
+    cct::Multimap<int,AAA> map;
+
+    QVariant v=QVariant::fromValue( map );
+    auto it=v.value<QAssociativeIterable>();
+}
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    std::multimap<int,double> ssfe2;
+    
+    fxx777();
+    fxx111({ "123",123,"dsfa" });
+    fxx222();
+    fxx333();
+    fxx444();
 
     MainWindow win;
     win.show();
